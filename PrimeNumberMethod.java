c17fda9d1844ef805ea21bd0a987ef2ee5fbb194
MWessels62 <m.wessels62@gmail.com>
2019-08-29 16:43:59 +0200

    Added comments, fixed minor inefficiences, and made the program loop to continue prompting the user for new entries

--- PrimeNumberMethod.java
+++ PrimeNumberMethod.java
@@ -7,6 +7,7 @@ public class PrimeNumberMethod {
 	//default value, will change to false if value is found to be divisible by anything other than 1 or itself
 	boolean prime = true;
 	
+	//Determines whether or not a number is prime 
 	public boolean isPrime(int inputNum) {
 		for (int i = 2; i < inputNum && i < 10; i++ ){
 			if (inputNum % i == 0) {
@@ -19,20 +20,24 @@ public class PrimeNumberMethod {
 	public static void main(String[] args) {
 		PrimeNumberMethod newTest = new PrimeNumberMethod();
 		
-		Scanner input = new Scanner(System.in);
-
 		//Get integer from user
-		System.out.println("Type in an integer to check if it is prime... ");
-		int userInput = input.nextInt(); 
+		Scanner input = new Scanner(System.in);
+		int userInput = 1;
 		
-		//Determine output string
-		String outputMessage;
-		if (newTest.isPrime(userInput) == true) {
-			outputMessage = "IS a prime number";
-		} else {
-			outputMessage = "IS NOT a prime number";
+		while (userInput != 0) {
+			System.out.println("\nType in an integer to check if it is prime... (Zero/0 to exit): ");
+			userInput = input.nextInt(); 
+
+			//Determine output string
+			String outputMessage;
+			if (newTest.isPrime(userInput) == true) {
+				outputMessage = "IS a prime number";
+			} else {
+				outputMessage = "IS NOT a prime number";
+			}
+			System.out.println("The number you entered (" + userInput + ") " + outputMessage);
 		}
-		System.out.println("The number you entered (" + userInput + ") " + outputMessage);
+		input.close();
 	}
 
 }
